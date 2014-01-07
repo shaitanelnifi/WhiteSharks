ccdcf495c153d6f84b387dc4c8f4c5e88b0cab40
John Mai <jxmai@ucsc.edu>
2014-01-06 18:06:06 -0800

    sdfdf
    sdfsdf

---
+++ Assets/codes/con1.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class con1 : MonoBehaviour {
+	private static bool click = false;
+	public static bool Click{
+		get{
+			return click;
+		}
+		set{
+			click = value;
+		}
+		
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool flag = npc1.Click;
+		if (flag){
+			npc1.Click = false;
+			click = true;
+		}
+		if (click) {
+			renderer.enabled = true;
+			collider.enabled = true;
+		}
+	}
+	void OnMouseDown(){
+		if (Input.GetMouseButton (0)) {
+			click = false;
+			renderer.enabled = false;
+			collider.enabled = true;
+		}
+	}
+}
---
+++ Assets/codes/move.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+	// Use this for initialization
+	public KeyCode moveLeft;
+	public KeyCode moveRight;
+	public float speed = 0.5f;
+	public Camera mainCam;
+	public Transform player;
+
+
+	void OnTriggerEnter2D(Collider2D collider){
+		activate act = collider.gameObject.GetComponent<activate> ();
+		if (act != null) {
+			Vector3 temp = transform.position; 
+			temp.x = -10.86f;
+			temp.y = -1.217f;
+			player.position = temp;
+			//Vector3 temp2 = transform.position; 
+			Vector3 temp2 = new Vector3(-17.873f,-0.9213791f,-10f);
+			mainCam.transform.position = temp2;
+
+			//mainCam.camera.transform.position.x = -17.873;
+
+		}
+
+	
+	}
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(moveLeft))
+			transform.Translate(new Vector3(1,0, 0)* -speed * Time.deltaTime);
+		if(Input.GetKey(moveRight))
+			transform.Translate(new Vector3(1,0, 0)* speed * Time.deltaTime);
+	}
+}
