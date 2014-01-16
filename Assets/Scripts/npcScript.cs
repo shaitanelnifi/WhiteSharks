61b439e2a5c4bec7596e3f6a53d65914955bbbd7
John Mai <jxmai@ucsc.edu>
2014-01-15 18:30:27 -0800

    fixed clicking bug
    fixed clicking bug

--- Assets/Scripts/npcScript.cs
+++ Assets/Scripts/npcScript.cs
@@ -10,6 +10,8 @@ using System.Collections;
 public class npcScript : MonoBehaviour {
 	
 	public GameObject conversation, playerObj;
+	public BoxCollider2D box;
+
 
 	//enable conversation object if left mouse button is clicked.
 	void OnMouseDown(){
@@ -23,9 +25,11 @@ public class npcScript : MonoBehaviour {
 		if (transform.position.y < playerObj.transform.position.y) {
 			renderer.sortingLayerName= "foreground";
 			renderer.sortingOrder = 2;
+			box.isTrigger = true;
 		}
 		else{
 			renderer.sortingLayerName= "middleground";
+			box.isTrigger = false;
 		}
 	}
 }
