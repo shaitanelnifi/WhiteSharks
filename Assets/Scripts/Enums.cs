c17b1f0662c83bdd7cbec4b1264d2eceedbf01fb
Maria Lopez-Latorre <mentatzoe@gmail.com>
2014-01-20 20:09:43 -0800

    Added enums script
    Added a script that contains the enums for GuiltLevel and Category used
    in CaseElements

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enums : MonoBehaviour {
+
+	enum GuiltLevel
+	{
+		guilty,
+		suspect,
+		witness
+	};
+
+	enum Category
+	{
+		WeaponType1,
+		WeaponType2,
+		WeaponType3,
+		WeaponType4,
+		PersonalItem
+	};
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
