14c7183a46e79563bd4f3562cf5936d4decd82c4
Maria Lopez-Latorre <mentatzoe@gmail.com>
2014-01-20 20:25:30 -0800

    Fixed duplicate enums
    Enums were twice on the script

--- Assets/Scripts/Enums.cs
+++ Assets/Scripts/Enums.cs
@@ -1,49 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-<<<<<<< HEAD
 public class Enums : MonoBehaviour {
 
 	enum GuiltLevel
 	{
-		guilty,
+		guilty=1,
 		suspect,
 		witness
 	};
 
 	enum Category
 	{
-		WeaponType1,
+		WeaponType1=1,
 		WeaponType2,
 		WeaponType3,
 		WeaponType4,
 		PersonalItem
 	};
-
-
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
-=======
-public enum Category {
-	WeaponType1 = 1,
-	WeaponType2,
-	WeaponType3,
-	WeaponType4,
-	PersonaItem
-};
-
-public enum GuiltLevel {
-	Guilty = 1,
-	Suspect,
-	Witness
-};
->>>>>>> 387d5b74d8b87077ff41f8feb9758fcca49ef57b
