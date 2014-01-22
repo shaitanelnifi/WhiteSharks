604a7839f7b35462b215c5b2c377e7cebf314923
John Mai <jxmai@ucsc.edu>
2014-01-21 19:25:58 -0800

    code cleaning
    cleaning code

--- Assets/Scripts/CaseObject.cs
+++ Assets/Scripts/CaseObject.cs
@@ -7,10 +7,10 @@ public class CaseObject : CaseElement {
 	public ArrayList infoGuilty;	//Store a list of strings that might be displayed if the item is related to the murder
 	public ArrayList infoNotGuilty; //If the item is unrelated, you might display one or more of these strings
 
-	//enable conversation object if left mouse button is clicked.
+/*	//enable conversation object if left mouse button is clicked.
 	public override void onMouseDown(){
 		//Does nothing at the moment
-	}
+	}*/
 
 	public void addInfoGuilty(string newInfo){
 		infoGuilty.Add(newInfo);
