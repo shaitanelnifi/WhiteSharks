bd787bf4ff65f625eaae1db540a8b103f240c3fe
John Mai <jxmai@ucsc.edu>
2014-01-30 21:57:08 -0800

    Adrian's code
    Adrian's code

--- Assets/Scripts/journal.cs
+++ Assets/Scripts/journal.cs
@@ -7,6 +7,7 @@ public class journal : MonoBehaviour {
 
 	private static journal instance;
 	private static int MAX_NPC = 3;
+	private static journal j;
 
 	public static journal Instance {
 		get {
@@ -17,130 +18,172 @@ public class journal : MonoBehaviour {
 			return instance;
 		}
 	}
-	//PoI buttons
-	public GameObject poI2;
 
 	public List<NPC> personsOfInterest;
+	public ArrayList weaponList;
 
-	//Test case elements of NPC type
+	//Defaults for non-visible NPC
 	public static Sprite emptyPortrait;
 	private string emptyName;
-	public NPC npc1;
-	public NPC npc2;
-	public NPC npc3;
 
 	//Grab view tab buttons. Will change to use gameobject find.
 	public GameObject viewTab1;
 	public GameObject viewTab2;
 	public GameObject viewTab3;
 
-	//Grab buttons and textfield from view. Will change to use gameobject find.
+	//Grab buttons and textfield from view. Will change to use gameobject find. Three lists for three different types of buttons.
+	private static List<GameObject> viewTabList;
 	private static List<GameObject> poiButtonList;
-	public GameObject poiButton1;
-	public GameObject poiButton2;
-	public GameObject poiButton3;
-	public UILabel poiName;
-	public UILabel poiDescription;
+	public static List<GameObject> objectButtonList;
 
-	// Use this for initialization
+	public GameObject poiGrid;
+	public GameObject objectGrid;
+
+	public UILabel nameLabel;
+	public UILabel descriptionLabel;
+	
+	public GameObject poiView;
+	public GameObject mapView;
+
+	//Destroys duplicate UI Roots.
+	void Awake () {
+		if(!j){
+			j = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else {
+			Destroy (gameObject);
+		}
+	}
+	
 	void Start () {
 		//Default name for "invisible" person of interest.
 		emptyName = "?????";
 
-		//Person of interest list.
+		//Persons of interest list.
 		personsOfInterest = GameManager.npcList;
 
-		//Listens for tab button presses in journal and runs onClick with button clicked as parameter.
-		/*UIEventListener.Get (viewTab1).onClick += this.onTabClick;
-		UIEventListener.Get (viewTab2).onClick += this.onTabClick;
-		UIEventListener.Get (viewTab3).onClick += this.onTabClick;*/
+		//Weapon list once that's ready.
+		//weaponList = GameManager.weaponList;
 
-		//Listens for button presses in poiView and runs onClick with button clicked as parameter.
-		UIEventListener.Get(poiButton1).onClick += this.onClick;
-		UIEventListener.Get(poiButton2).onClick += this.onClick;
-		UIEventListener.Get(poiButton3).onClick += this.onClick;
+
+		//Listens for tab button presses in journal and runs onClick with button clicked as parameter.
+		UIEventListener.Get (viewTab1).onClick += this.onClick;
+		UIEventListener.Get (viewTab2).onClick += this.onClick;
+		UIEventListener.Get (viewTab3).onClick += this.onClick;
+		//Want to get rid of this too.
+		viewTabList = new List<GameObject>();
+		viewTabList.Add(viewTab1);
+		viewTabList.Add(viewTab2);
+		viewTabList.Add(viewTab3);
 
 		//List of person of interest portrait buttons.
 		poiButtonList = new List<GameObject>();
-		poiButtonList.Add (poiButton1);
-		poiButtonList.Add (poiButton2);
-		poiButtonList.Add (poiButton3);
+		objectButtonList = new List<GameObject>();
 
 		initPoIView ();
+		initObjView ();
+		changeView (0);
 	}
 
-	void Update () {
+	//Single onclick function for any button in the journal.
+	void onClick(GameObject button){
+		if(viewTabList != null && viewTabList.Contains(button)){
+			changeView (viewTabList.IndexOf(button));
+			Debug.Log ("won't happen yet");
+		}
+		else if(poiButtonList.Contains(button)){
+			changePOI(poiButtonList.IndexOf(button));
+			Debug.Log ("poiButton!");
+		}
+		else if (objectButtonList.Contains (button)){
+			changeObject(objectButtonList.IndexOf(button));
+			Debug.Log ("objectbutton!");
+		}
 	}
 
-	//Change journal view by tab buttons.
-	//Should have onClick moved into it to provide one general on click event for any button if possible.
-	void onTabClick(GameObject button){
-		if (button == viewTab1) {
-		} 
-		else if (button == viewTab2) {
+	//----- Button type functions
+	//Changes view when view tab is clicked.
+	//Will make helper function for grid/view SetActive.
+	void changeView(int viewNumber){
+		switch (viewNumber) {
+			case 0://PoI
+				mapView.SetActive(false);
+				poiView.SetActive(true);
+				objectGrid.SetActive(false);
+				poiGrid.SetActive(true);
+				break;
+			case 1://Object
+				mapView.SetActive (false);
+				poiView.SetActive(true);
+				objectGrid.SetActive(true);
+				poiGrid.SetActive(false);
+				break;
+			case 2://Map
+				mapView.SetActive (true);
+				poiView.SetActive(false);
+				objectGrid.SetActive(false);
+				poiGrid.SetActive(false);
+				break;
+		}
+		clearLabels ();
+	}
+
+	//Changes PoI when a PoI portrait is clicked.
+	void changePOI(int poiNumber){
+		if(personsOfInterest[poiNumber].isVisible()){
+			nameLabel.text = personsOfInterest[poiNumber].getElementName();
+			descriptionLabel.text = personsOfInterest[poiNumber].getDescription();
 		}
 		else {
+			nameLabel.text = emptyName; 
+			descriptionLabel.text = emptyName;
 		}
 	}
 
-	//Needs clean up (repetitive). Needs to be general to work with both PoI view and object view
-	void onClick(GameObject button){
-		if (button == poiButton1) {
-			Debug.LogError(GameManager.npcList[0].isVisible());
-			if(GameManager.npcList[0].isVisible()){
-				poiName.text = personsOfInterest[0].getElementName();
-				poiDescription.text = personsOfInterest[0].getDescription();
-			}
-			else {
-				poiName.text = emptyName; 
-				poiDescription.text = emptyName;
-			}
+	//Changes object/weapon being viewed when portrait is clicked.
+	void changeObject(int objectNumber){
+		//Code for grabbing weapon names and descriptions.
+		/*if(weaponList[objectNumber].isVisible()){
+			nameLabel.text = weaponList[objectNumber].getElementName();
+			descriptionLabel.text = weaponList[objectNumber].getDescription();
 		}
-		else if(button == poiButton2) {
-			if(GameManager.npcList[1].isVisible()){
-				poiName.text = personsOfInterest[1].getElementName();
-				poiDescription.text = personsOfInterest[1].getDescription();
-			}
-			else {
-				poiName.text = emptyName; 
-				poiDescription.text = emptyName;
-			}
-		}
-		else if(button == poiButton3) {
-			if(GameManager.npcList[2].isVisible()){
-				poiName.text = personsOfInterest[2].getElementName();
-				poiDescription.text = personsOfInterest[2].getDescription();
-			}
-			else {
-				poiName.text = emptyName; 
-				poiDescription.text = emptyName;
-			}
-		}
+		else {
+			nameLabel.text = emptyName; 
+			descriptionLabel.text = emptyName;
+		}*/
 	}
 
 	//Initialize PoI view.
 	public void initPoIView(){
+		//Add buttons to poi button list and put them in UI event listener.
+		foreach (Transform child in poiGrid.transform){
+			UIEventListener.Get(child.gameObject).onClick += this.onClick;
+			poiButtonList.Add(child.gameObject);
+		}
+
 		for (int i = 0; i < personsOfInterest.Count; i++) {
-			Debug.Log (i);
 			if(personsOfInterest[i] != null){
-				Debug.Log("getting image " +i+ ": " + personsOfInterest[i].getProfileImage ().ToString ());
-				//GetComponent is slow, will use gameobject find component in children in later iteration.
 				poiButtonList[i].gameObject.GetComponent<UI2DSprite>().sprite2D = personsOfInterest[i].getProfileImage();
 			}
 			else {
-				Debug.LogError("Im null");
 				poiButtonList[i].gameObject.GetComponent<UI2DSprite>().sprite2D = emptyPortrait;
 			}
 		}
 	}
 
-	public void changePoIView(NPC n){
-		//if (indexPoI <= MAX_NPC - 1) {
-		//	personsOfInterest[i]=n;
-			Debug.Log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-		//	Debug.Log("getting image " +i+ ": " + personsOfInterest[i].getProfileImage ().ToString ());
-		//	poiButtonList[i].gameObject.GetComponent<UI2DSprite>().sprite2D = personsOfInterest[i].getProfileImage();
-		//}
+	//Initialize obj view.
+	public void initObjView(){
+		//Add buttons to obj button list and put them in UI event listener.
+		foreach (Transform child in objectGrid.transform){
+			UIEventListener.Get(child.gameObject).onClick += this.onClick;
+			objectButtonList.Add(child.gameObject);
+		}
+	}
 
+	//Clear description labels. Might rename and add obj/poi grid on/off.
+	void clearLabels(){
+		nameLabel.text = "";
+		descriptionLabel.text = "";
 	}
 }
