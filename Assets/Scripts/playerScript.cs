add84d3246b537502a6fb777cf1aa9d628755419
John Mai <jxmai@ucsc.edu>
2014-02-12 23:41:33 -0800

    pathfinding
    asdasd

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
 player class. controls the movement of the main character.
 
 changes: change scene depends on which door the player collide with. -John Mai 1/12/2014
@@ -14,9 +14,84 @@ public class playerScript : CaseElement {
 	public Transform mainChar;
 	bool facingLeft = true;
 	Animator anim;
+	Vector2 targetPosition;
+	Vector2 direction;
+	Vector2 closestColl;
 
 	void Start(){
 		anim = GetComponent<Animator>();
+	}
+	void FixedUpdate(){	
+		float distance;
+		if(Input.GetMouseButton(0)){
+			//get mouse clicked location and convert them to world point.
+			targetPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+			Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(targetPosition.x, targetPosition.y, camera.nearClipPlane));
+			targetPosition.x = mousePosition.x;
+			targetPosition.y = mousePosition.y;
+		}
+
+		if (targetPosition.x != 0){
+			//if something is on the way, use find path
+			if (Physics2D.Linecast(transform.position, targetPosition)){	
+				//didnt called
+				if(objectOnWay(targetPosition)){
+					Vector2 toPoint = FindClosestPoint().transform.position;
+					distance = Vector2.Distance (transform.position, toPoint);
+					transform.position = Vector2.Lerp (transform.position, toPoint,Time.deltaTime* (maxSpeed/distance));
+				}
+			}
+			//else go straight to that location
+			else{
+				distance = Vector2.Distance (transform.position, targetPosition);
+				if(distance > 0){
+					transform.position = Vector2.Lerp (transform.position, targetPosition,Time.deltaTime* (maxSpeed/distance));
+				}
+			}
+		}
+	/*	else if (targetPosition.x == 0){
+			Debug.Log ("not suppose to show");
+		}
+		//didnt get into here
+		else{
+			distance = Vector2.Distance (transform.position, targetPosition);
+			Debug.Log ("3333");
+			if(distance > 0){
+				Debug.Log ("4444");
+				transform.position = Vector2.Lerp (transform.position, targetPosition,Time.deltaTime* (maxSpeed/distance));
+			}
+		}*/
+	}
+
+	//returns true if the collide object is type PolygonCollider2D
+	public bool objectOnWay(Vector2 target){
+		bool result = false;
+		Vector2 direct;
+		direct.x = target.x - transform.position.x;
+		direct.y = target.y - transform.position.y;
+		//get the info of that object.
+		RaycastHit2D tempHit = Physics2D.Raycast(transform.position, direct);
+		if (tempHit.collider.GetType() == typeof(PolygonCollider2D)){
+			result = true;
+		}
+		return result;		 
+	}
+	//return closest pathPoint near player.
+	GameObject FindClosestPoint() {
+		GameObject[] points;
+		points = GameObject.FindGameObjectsWithTag("point");
+		GameObject closest= null;
+		float distance = Mathf.Infinity;
+		Vector2 position = transform.position;
+		foreach (GameObject point in points) {
+			float curDistance = Vector2.Distance(point.transform.position,position);
+			if (curDistance < distance) {
+				closest = point;
+				distance = curDistance;
+			}
+		}
+		Debug.Log ("closet point: " + closest.transform.position);
+		return closest;
 	}
 	//change scene when collide with door
 	void OnTriggerEnter2D(Collider2D collider){
@@ -40,17 +115,6 @@ public class playerScript : CaseElement {
 		}
 	}
 
-	void FixedUpdate(){
-		float move = Input.GetAxis ("Horizontal");
-		float moveVer = Input.GetAxis ("Vertical");
-		rigidbody2D.velocity = new Vector2 (move * maxSpeed, moveVer * maxSpeed);
-		anim.SetFloat("Speed",Mathf.Abs(move));
-
-		if(move < 0 && !facingLeft)
-			Flip();
-		else if(move>0 && facingLeft)
-			Flip();
-	}
 
 	//flips the sprite or animation
 	void Flip(){
@@ -63,3 +127,20 @@ public class playerScript : CaseElement {
 		Destroy (Scene.player);
 	}
 }
+	
+
+
+/*
+//old update function
+void FixedUpdate(){
+	float move = Input.GetAxis ("Horizontal");
+	float moveVer = Input.GetAxis ("Vertical");
+	rigidbody2D.velocity = new Vector2 (move * maxSpeed, moveVer * maxSpeed);
+	anim.SetFloat("Speed",Mathf.Abs(move));
+	
+	if(move < 0 && !facingLeft)
+		Flip();
+	else if(move>0 && facingLeft)
+		Flip();
+}
+*/
