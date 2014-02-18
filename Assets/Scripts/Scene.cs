1b76584c6b238966e97ab2820647b3acda9f7a9e
Maria Lopez-Latorre <mentatzoe@gmail.com>
2014-02-18 15:56:03 -0800

    Updating stuff
    So that we can finally merge everything that should be merged.

--- Assets/Scripts/Scene.cs
+++ Assets/Scripts/Scene.cs
@@ -6,7 +6,7 @@ using System.Collections;
 
 public class Scene : MonoBehaviour {
 	public static GameObject player;
-	public int id;
+	public float scaleX, scaleY;
 	// Use this for initialization
 	void Start () {
 		string temp = (string)GameManager.Instance.GetMainCharacter ();
@@ -15,22 +15,9 @@ public class Scene : MonoBehaviour {
 		tempVec.x = GameManager.Instance.GetNextX ();
 		tempVec.y = GameManager.Instance.GetNextY ();
 		player.transform.position = tempVec;
-		switch (id) {
-			case 2:
-				player.transform.localScale = new Vector3(0.37f,0.37f,0.37f);
-				break;
-			case 3:
-				player.transform.localScale = new Vector3(0.48f,0.48f,0.48f);
-				break;
-			case 4:
-				player.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
-				break;
-			case 5:
-				player.transform.localScale = new Vector3(1f,1f,1f);
-				break;
-			case 6:
-				player.transform.localScale = new Vector3(0.8f,0.8f,0.8f);
-				break;
+		//scale the player sprite
+		if (scaleX != 0 && scaleY != 0) {
+			player.transform.localScale = new Vector3(scaleX, scaleY, 1);	
 		}
 	}
 }
