7a8eee67b8213772835bbedbabbf30ee725846a1
Maria Lopez-Latorre <mentatzoe@gmail.com>
2014-03-14 13:14:08 -0700

    Tweaking intro
    Added fadeIn/out.
    
    Coding is very crappy though, it might need MORE tweaking.

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -7,6 +7,10 @@ public class Intro : MonoBehaviour {
 	Color a;
 	bool started = false;
 	bool theEnd = false;
+	bool change = false;
+	bool fadeOut = true;
+	bool start = true;
+	bool fadeInDone = false;
 	public Sprite next;
 	// Use this for initialization
 	void Start () {
@@ -18,40 +22,69 @@ public class Intro : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (r.color.a < 1) {
+		if (r.color.a < 1 && start) {
 						a.a += 0.01f;
 						r.color = a;
 				} else if (!started) {
+			start = false;
 						started = true;
 						initDialogue(0);
 		}
 
 		if (GameManager.dialogueJustFinished) {
 			GameManager.dialogueJustFinished = false;
-			change ();
+			if (!theEnd){
+				//Debug.Log ("emagherd");
+				change = true;
+
+			} else {
+				StartCoroutine("wait");
+
+			}
 		}
 
+		if (change) {
+			Debug.Log ("In Change " + r.color.a + " " + fadeOut);
+			if (r.color.a >= 0 && fadeOut) {
+					Debug.Log ("Fading out");
+					a.a -= 0.01f;
+					r.color = a;
+			} 
+			else {
+				fadeOut = false;
+				r.sprite = 	next;
+					if (r.color.a < 1) {
+					Debug.Log ("a " + a.a);
+					Debug.Log ("r " + r.color.a);
+					a.a += 0.01f;
+					r.color = a;
+					} else {
+					fadeInDone = true;
+				}
+			}
+			if (fadeInDone){
+				r.color = a;
+				StartCoroutine("test");
+				theEnd = true;	
+				change = false;
+			}
+		}
 	}
 
 	IEnumerator test(){
-		yield return new WaitForSeconds (3);
+		yield return new WaitForSeconds (2);
 		initDialogue (6);
+	}
+
+	IEnumerator wait(){
+		yield return new WaitForSeconds (2);
+		Application.LoadLevel("stage1");
 	}
 
 	void initDialogue(int num){
 		Debug.Log ("aloha");
 		Dialoguer.StartDialogue(num);
 	}
-
-	void change(){
-		if (!theEnd){
-			Debug.Log ("emagherd");
-			r.sprite = 	next;
-			StartCoroutine("test");
-			theEnd = true;
-		} else {
-			Application.LoadLevel("stage1");
-		}	
-	}
 	
+
 }
