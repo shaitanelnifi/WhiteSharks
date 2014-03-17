4320d8e8e6e7332026bb437282147f3f6f270573
Maria Lopez-Latorre <mentatzoe@gmail.com>
2014-03-17 00:10:07 -0700

    Office code
    And such.

--- Assets/Scripts/Office.cs
+++ Assets/Scripts/Office.cs
@@ -7,9 +7,9 @@ using System.Collections;
  * We'll probably want to change the way it's implemented later.
  */
 public class Office : MonoBehaviour {
-
-	private bool firstTime = true;
+	
 	public bool correctAnswer = false;
+	private bool started = false;
 	// Use this for initialization
 	void Start () {
 	
@@ -17,31 +17,42 @@ public class Office : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (firstTime) {
-						StartCoroutine ("firstDialogue");
-		} else if (!GameManager.dialogueJustFinished) {
+		if (GameManager.firstTimeOffice && !started) {
+			Debug.Log("Hey");
+			StartCoroutine ("firstDialogue");
+		} else if (!GameManager.firstTimeOffice && !started) {
+			Debug.Log("NO Hey");
 			StartCoroutine("secondDialogue");
 		}
-		if (GameManager.dialogueJustFinished && firstTime) {
+
+		if (GameManager.dialogueJustFinished) {
 			GameManager.dialogueJustFinished = false;
-		} else if (GameManager.dialogueJustFinished && !firstTime){ 
-			GameManager.dialogueJustFinished = false;
+			GameManager.firstTimeOffice = false;
+			StartCoroutine("goToPlaza");
+		} //else if (GameManager.dialogueJustFinished && !firstTime){ 
 			//We have to add something that checks if you've answered correctly
-			if (correctAnswer){
-				GameManager.Instance.finishEpisode();
-			} else {
-				Application.LoadLevel("finplaza");
-			}
-		}
+			//if (correctAnswer){
+			//	GameManager.Instance.finishEpisode();
+			//} else {
+			//	StartCoroutine("goToPlaza");
+			//}
+		//}
 	}
 
 	public IEnumerator firstDialogue(){
+		started = true;
 		yield return new WaitForSeconds (1.5f);
 		Dialoguer.StartDialogue(2);
 	}
 
 	public IEnumerator secondDialogue(){
+		started = true;
 		yield return new WaitForSeconds (1.5f);
 		Dialoguer.StartDialogue(5);
+	}
+
+	public IEnumerator goToPlaza(){
+		yield return new WaitForSeconds (1.5f);
+		Application.LoadLevel("finplaza");
 	}
 }
